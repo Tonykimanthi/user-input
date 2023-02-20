67c4707aceca718e00ab2cfcdcebfb41871c2cc9
Tony Km <125383511+Tonykimanthi@users.noreply.github.com>
2023-02-20 13:27:34 +0300

    User input web page
    This was created during learning session

---
+++ main.js
@@ -0,0 +1,6 @@
+let name;
+
+document.getElementById("myBtn").onclick = function(){
+    name = document.getElementById("myText").value;
+    document.getElementById("label").innerHTML = "Hello " + name;
+}
